19da92e332f415f6fdad8867580217a5e886f058
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-10-19 11:45:22 +0200

    Add docstring and test cases in funcions

--- participants/Cadiou/pr_tutorial/simple_functions.py
+++ participants/Cadiou/pr_tutorial/simple_functions.py
@@ -1,12 +1,49 @@
-def fibonacci(max):
+from typing import List
+
+
+def fibonacci(vmax: int) -> List[int]:
+    """Compute the fibonnaci sequence up to max.
+    
+    Argument:
+    ---------
+    vmax: int, positive
+        The maximum value of the sequence.
+
+    Returns:
+    --------
+    fibs: list of int
+        The list of fibonacci numbers.
+    """
+    if vmax == 0:
+        return [0]
+    elif vmax == 1:
+        return [0, 1]
+    elif vmax < 0:
+        raise ValueError('vmax must be positive')
+
     values = [0, 1]
-    while values[-2] + values[-1] < max:
+    while values[-2] + values[-1] < vmax:
         values.append(values[-2] + values[-1])
+
     return values
 
 
-def factorial(value):
-    if value == 0:
+def factorial(value: int) -> int:
+    """Compute the factorial of a value.
+    
+    Argument:
+    ---------
+    value: int, positive
+        The value to compute the factorial of.
+    
+    Returns:
+    --------
+    fact: int
+        The factorial of the value.
+    """
+    if value < 0:
+        raise ValueError("value must be positive, got {}".format(value))
+    elif value == 0:
         return 1
     else:
         return value * factorial(value - 1)
