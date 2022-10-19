243de6516a67943306fa11c4b63a13113a5f8480
criselsuarez <20343857+criselsuarez@users.noreply.github.com>
2022-10-19 12:33:19 +0200

    adding test for prime numbers

--- participants/Suarez/pr_tutorial/tests/test_simple_function.py
+++ participants/Suarez/pr_tutorial/tests/test_simple_function.py
@@ -1,7 +1,14 @@
-from pr_tutorial.simple_functions import factorial
+from pr_tutorial.simple_functions import factorial, prime
 
 
 def test_factorial_3():
-    """Simplest test for one crete case"""
+    """Simplest test for one create case"""
 
     assert factorial(3) == 6
+
+
+def test_prime():
+  """Simplest test for one create case"""
+
+  assert prime(3) == True
+  assert prime(2) == False
