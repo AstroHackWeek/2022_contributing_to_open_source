0bc47829d48f243f51c0a3df4b901743a6d33e66
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2022-10-19 11:34:54 +0200

    Update simple_functions.py

--- participants/Fouesneau/pr_tutorial/simple_functions.py
+++ participants/Fouesneau/pr_tutorial/simple_functions.py
@@ -1,11 +1,15 @@
-def fibonacci(max):
+from typing import Sequence
+
+def fibonacci(max: int) -> List[int]:
+    """ Calculate the Fibonacci series up to max """
     values = [0, 1]
     while values[-2] + values[-1] < max:
         values.append(values[-2] + values[-1])
     return values
 
 
-def factorial(value):
+def factorial(value: int) -> int:
+    """ Calculate the factorial of a value """
     if value == 0:
         return 1
     else:
